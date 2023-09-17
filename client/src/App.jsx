fd6e0becad09abd4c3ba2a43071bf4d7364740e8
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-09-17 15:42:02 +0500

    Added routing and post show page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
-import PostsList from './features/posts/PostList'
+// import PostsList from './features/posts/PostList'
+import NavBar from './components/NavBar'
+import AppRoutes from './components/AppRoutes'
 
 function App() {
   
   return (
-    <>
+    <Router>
       <div>
         <h1>React on Rails Blog</h1>
 
-        <PostsList />
+        <NavBar />
+
+        {/* <PostsList /> */}
+        <AppRoutes />
       </div>
-    </>
+    </Router>
   )
 }
 
